472cb2394b14f235d8821bf968d889277a267987
Joseph <joseph.quiros.murillo@est.una.ac.cr>
2024-03-06 23:12:43 -0600

    create Calculator's GUI

---
+++ src/main/java/universidad/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package universidad.calculadora;
+
+/**
+ *
+ * @author Joseph
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
